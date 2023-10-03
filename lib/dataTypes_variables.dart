a84db36f792212b75d3b9e2f19d93c87072c5129
muhammadmuneebwaseem <muneeb@zenkoders.com>
2023-10-03 05:22:56 +0500

    data type

---
+++ lib/dataTypes_variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  var section;
+
+  section = "test";
+
+  print("section: $section");
+
+  String name = "ahmed";
+
+  print("name: $name");
+
+  var rollno = 10;
+
+  print("rollno $rollno");
+}
