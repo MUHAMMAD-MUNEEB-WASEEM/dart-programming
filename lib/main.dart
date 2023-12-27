b5ef01f978b8a3d20fbb6baf006e3cadeb8a74bb
muhammadmuneebwaseem <muneebwaseem78@gmail.com>
2023-12-27 15:00:07 +0500

    textTheme

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,13 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+          useMaterial3: true,
+          textTheme: TextTheme(
+              displayLarge:
+                  TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+              titleMedium:
+                  TextStyle(fontSize: 11, fontWeight: FontWeight.w500))),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -40,22 +44,23 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("First Flutter App1"),
         ),
-        body: Center(
-            child: CircleAvatar(
-                radius: 100,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 50,
-                          height: 50,
-                          child: Image.asset('../assets/images/image2.png'),
-                        ),
-                        Text("image name")
-                      ]),
-                ))));
+        body: Column(
+          children: [
+            Text(
+              "Heading Large",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Text(
+              "Heading Small",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Text(
+              "Heading Large",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Text("Heading Small",
+                style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ));
   }
 }
