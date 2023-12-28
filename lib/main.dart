6b7702f81ef7441c5aebbf36f246a3b8161a2049
muhammadmuneebwaseem <muneebwaseem78@gmail.com>
2023-12-28 23:03:12 +0500

    date time and setState

--- lib/main.dart
+++ lib/main.dart
@@ -40,19 +40,28 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var arrNames = ['muneeb', 'hammad', 'josh', 'john', 'doe'];
+    var time = DateTime.now();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: const Text("First Flutter App1"),
         ),
-        body: const Center(
-            child: Card(
-          shadowColor: Colors.blue,
-          elevation: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Text('Hello World!'),
-          ),
-        )));
+        body: Center(
+          child: Container(
+              width: 200,
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      'Current time is:, $time, current day is: ${time.day}, current month is: ${time.month}'),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text("Current time"))
+                ],
+              )),
+        ));
   }
 }
